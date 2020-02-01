4bb97904912241d4891c9424bf94162b7b1eb78b
Yashvishnoi <jivishnoi@gmail.com>
2020-02-01 12:01:07 +0530

    function s.append

---
+++ abdul34.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+#include<string.h>
+#include<stdlib.h>
+#include<string>
+using namespace std;
+int main()
+{
+string str;
+getline(cin,str);
+str.append("Enough");
+cout<<str<<endl;
+return 0;
+}
