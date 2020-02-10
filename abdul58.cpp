81e66a7e7e2a363ed97435214a4e3bb93b08f30b
Yashvishnoi <jivishnoi@gmail.com>
2020-02-10 22:26:54 +0530

    function overloading

---
+++ abdul58.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class parent
+{
+public:
+void display()
+{
+cout<<"Display of parent"<<endl;
+}
+};
+
+
+class child:public parent
+{
+public:
+void display()
+{
+cout<<"Dispaly  from child";
+}
+};
+
+int main()
+{
+parent p;
+p.display();
+child c;
+c.display();
+return 0;
+}
