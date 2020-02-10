7001f5d2d4cf703af72e5b174c87544d46fe032a
Yashvishnoi <jivishnoi@gmail.com>
2020-02-10 23:11:46 +0530

    virtual keyword in calling overriding method

--- abdul59.cpp
+++ abdul59.cpp
@@ -1,4 +1,4 @@
- 
+/* 
 #include<iostream>
 using namespace std;
 class base
@@ -24,8 +24,33 @@ int main()
 base *ptr=new derived();//function i is called
 ptr->fun();
 return 0;
+}*/
+
+#include<iostream>
+using namespace std;
+class base
+{
+public:
+virtual void fun()//(i) Virtual- it is a keyword used to run program in real life
+{
+cout<<"function of Base";
 }
+};
 
+class derived:public base
+{
+public:
+void fun()//(ii)
+{
+cout<<"Function of derived ";
+}
+};
 
+int main()
+{
+base *ptr=new derived();//function i is called
+ptr->fun();
+return 0;
+}
 
 
