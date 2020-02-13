d60604754f78c8ce564f3b46df2c4396d636f9bf
Yashvishnoi <jivishnoi@gmail.com>
2020-02-13 22:41:02 +0530

    Modified program 2 of abstract class

--- abdul64.cpp
+++ abdul64.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std;
-class base//use of this class is (i)-reusability (ii)-overwriting helps achiving polymorphism
+class base//use of this class  earlier is (i)-reusability (ii)-overwriting helps achiving polymorphism //now its use is only for achiveing polymorphism
 {
 public:
-void fun1()//concrete function 
-{
-cout<<"fun1 from base"<<endl;
-}
-virtual void fun2()=0;//pure virtual function
+virtual void fun1()=0;
+/*{
+cout<<"fun1 from base"<<endl;//-i concrete function //-i 
+}*/
+virtual void fun2()=0;// ii-pure virtual function it shows it must be overwriti by derived class
 
 };
-class derived:public base
+class derived:public base// derived class overwrite function ii
 {
 public:
+void fun1()
+{
+cout<<"Fun1 of derived classs"<<endl;
+}
 void fun2()
 {
 cout<<"fun2 of derived class"<<endl;
@@ -20,8 +24,11 @@ cout<<"fun2 of derived class"<<endl;
 };
 int main()
 {
-derived d;
-d.fun1();
-d.fun2();
+base *p=new derived();
+p->fun1();
+p->fun2();
+//derived d;
+//d.fun1();
+//d.fun2();
 return 0;
 }
