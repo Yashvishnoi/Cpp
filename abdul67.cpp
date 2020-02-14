09c2aeaf951a441e97d8629d2d11364397b1a88b
Yashvishnoi <jivishnoi@gmail.com>
2020-02-14 23:00:39 +0530

    static data member and function member

--- abdul67.cpp
+++ abdul67.cpp
@@ -13,9 +13,14 @@ a=10;
 b=10;
 count++;
 }
+static int getcount()//static member function -it can access only static data member of class it cannot able to access non static member 
+{
+return count;
+}
 };
 int test::count=0;
 
+/*//this main function is for static data member
 int main()
 {
 test t1,t2;
@@ -23,4 +28,15 @@ cout<<t1.count<<endl;
 cout<<t2.count<<endl;
 cout<<test::count;//static datamember can be access using object name or class name also if it is public
 return 0;
+}*/
+
+
+//this main function is for static member function
+int main()
+{
+cout<<test::getcount();
+test t1;
+cout<<t1.getcount();
+return 0;
 }
+
