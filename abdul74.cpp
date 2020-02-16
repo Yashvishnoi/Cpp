404252ef477f1260634079e4c104a88635568b13
Yashvishnoi <jivishnoi@gmail.com>
2020-02-16 20:04:49 +0530

    Modified the throw in exception handling

--- abdul74.cpp
+++ abdul74.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
+class myexception: exception
+{
 
+};
+int div(int a,int b)throw(myexception)//if you write throw() it means function is not throwing anything and it is optional not mandatory
+{
+if(b==0)
+throw myexception();
+return a/b;
+}
 int main()
 {
 int x,y,z;
@@ -8,19 +17,19 @@ cout<<"Enter the values of x,y"<<endl;
 cin>>x>>y;
 try
 {
-if(y==0)
+//if(y==0)
 //throw 1://(i)   //instead of 1 i acn throw any value
 //throw 1.5;//(ii)
-throw string("Div by zero"); //(iii)
-
-z=x/y;
+//throw string("Div by zero"); //(iii)
+//throw myexception();
+z=div(x,y);
 cout<<x<<endl;
 }
 //catch(int e)   //(i)
 //catch(double e) //(ii)
-catch (string e)//(iii)
+catch (myexception e)//(iii)
 {
-cout<<"Divison by zero \t"<<e<<endl;
+cout<<"Divison by zero \t"<<endl;
 }
 cout<<"Bye"<<endl;
 return 0;
