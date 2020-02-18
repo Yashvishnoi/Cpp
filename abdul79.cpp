a661cbc39eee399fabc08135c5a2bbe94a1be6ce
Yashvishnoi <jivishnoi@gmail.com>
2020-02-18 22:08:00 +0530

    2nd use of const

--- abdul79.cpp
+++ abdul79.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
 int x=10;
-int const *ptr=&x;
+int const *ptr=&x;//now it is pointing on x
 
 int y=20;
 ptr=&y;// now it will pointing on y  you can change pointer to point on other data member
-//++*ptr;// now also we cannnot modify because pointer is of type const
+//++*ptr;// now also we cannnot modify because pointer is of type constant
 cout<<*ptr<<endl;
 return 0;
 }
