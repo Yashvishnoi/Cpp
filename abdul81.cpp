366597e58a015d3bc3410c880bec8a7ac4ec4f89
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-18 22:45:21 +0530

    Update abdul81.cpp

--- abdul81.cpp
+++ abdul81.cpp
@@ -4,7 +4,9 @@ int main()
 {
 int x=10;//constant pointer to integer constant
 const int * const ptr=&x;
+  //++*ptr;//neither you can modify the data 
 int y=20;
+  //ptr=&y;//nor you can change the pointer
 cout<<*ptr<<" "<<x<<endl;
 return 0;
 }
