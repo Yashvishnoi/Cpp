b23fe169855d4331dc98848e29aaf768f4bd7781
Yashvishnoi <jivishnoi@gmail.com>
2020-02-21 22:48:57 +0530

    Writing and reading to a file

---
+++ abdul93.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+char data[100];
+ofstream outfile;
+outfile.open("file.txt");
+cout<<"Writing to a file"<<endl;
+cout<<"Enter your name"<<endl;
+cin.getline(data,100);
+outfile<<data<<endl;
+cout<<"Enter your Age";
+cin>>data;
+cin.ignore();
+outfile<<data<<endl;
+outfile.close();
+ifstream infile;
+infile.open("file.txt");
+cout<<"Reading from the file"<<endl;
+infile>>data;
+cout<<data<<endl;
+infile>>data;
+cout<<data<<endl;
+infile.close();	
+return 0;
+}
