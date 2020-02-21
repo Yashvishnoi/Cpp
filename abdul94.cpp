d3128b19b84fb76fb1800b49dc047d8ad5ea2844
Yashvishnoi <jivishnoi@gmail.com>
2020-02-21 23:44:06 +0530

    file handling

---
+++ abdul94.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ofstream outfile;
+string line;
+oflile.open("My.txt");
+while(My.txt)
+{
+cout<<"Enter your name and after that press 0"<<endl;
+getline(cin,line);
+if(line==0)
+break;
+ofile<<line<<endl;
+return 0;
+}
+outfile.close();
+
+ifstream infile;
+infile.open("My.txt");
+while(My.txt)
+{
+getline(infile,line);
+cout<<line<<endl;
+}
+infile.close();
+
+return 0;
+}
+//7906978010
