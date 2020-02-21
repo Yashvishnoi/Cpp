078538c3a84125e61928786010fdfacedc6162f2
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-21 22:51:40 +0530

    Update abdul93.cpp

--- abdul93.cpp
+++ abdul93.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 int main()
 {
-char data[100];
-ofstream outfile;
-outfile.open("file.txt");
-cout<<"Writing to a file"<<endl;
-cout<<"Enter your name"<<endl;
-cin.getline(data,100);
-outfile<<data<<endl;
-cout<<"Enter your Age";
-cin>>data;
-cin.ignore();
-outfile<<data<<endl;
-outfile.close();
-ifstream infile;
-infile.open("file.txt");
-cout<<"Reading from the file"<<endl;
-infile>>data;
-cout<<data<<endl;
-infile>>data;
-cout<<data<<endl;
-infile.close();	
-return 0;
+  char data[100];
+  ofstream outfile;
+  outfile.open("file.txt");
+  cout<<"Writing to a file"<<endl;
+  cout<<"Enter your name"<<endl;
+  cin.getline(data,100);
+  outfile<<data<<endl;
+  cout<<"Enter your Age";
+  cin>>data;
+  cin.ignore();
+  outfile<<data<<endl;
+  outfile.close();
+  ifstream infile;
+  infile.open("file.txt");
+  cout<<"Reading from the file"<<endl;
+  infile>>data;
+  cout<<data<<endl;
+  infile>>data;
+  cout<<data<<endl;
+  infile.close();	
+  return 0;
 }
