c764209bbd66a6f862a2f9c8024848efe0d53864
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-22 12:33:19 +0530

    Update structpointer.cpp

--- structpointer.cpp
+++ structpointer.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 struct point
 {
-int x,y;
+  int x,y;
 };
 int main()
 {
-struct point p1={10,20};
-struct point *p2=&p1;
-cout<<p2->x<<"\t"<<p2->y<<endl;
-return 0;
+  struct point p1={10,20};
+  struct point *p2=&p1;
+  cout<<p2->x<<"\t"<<p2->y<<endl;
+  return 0;
 }
