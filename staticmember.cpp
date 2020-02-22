52340feae637777886518093afd999021ee4b67f
Yashvishnoi <jivishnoi@gmail.com>
2020-02-22 19:31:39 +0530

    Use of static keyword

---
+++ staticmember.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class rectangle
+{
+private:
+
+int length,breadth,height;
+
+public:
+
+static int count;
+
+rectangle(int l,int b,int h)
+{
+length=l;
+breadth=b;
+height=h;
+count++;
+}
+
+int volume()
+{
+return length*breadth*height;
+}
+
+};
+
+int rectangle::count=0;
+
+int main()
+{
+int a,b,c;
+cout<<"Enter length,breadth,height"<<endl;
+cin>>a>>b>>c;
+rectangle r(a,b,c);
+rectangle r1(10,1,3);
+cout<<"Volume is "<<r.volume()<<endl;
+cout<<"Volume is "<<r1.volume()<<endl;
+cout<<rectangle::count<<"\t";
+return 0;
+}
