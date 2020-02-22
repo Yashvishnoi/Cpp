458ca19b2c6a3b2778a8da17809a40d9a2f49b69
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-22 23:18:29 +0530

    Update serializationInFile.cpp

--- serializationInFile.cpp
+++ serializationInFile.cpp
@@ -4,42 +4,42 @@
 using namespace std;
 class student
 {
-public:
-string name;
-int roll;
-string branch;
+  public:
+  string name;
+  int roll;
+  string branch;
 };
 
 int main()
 {
 
-student s1;
-cout<<"Enter your name"<<endl;
-cin>>s1.name;
-cout<<"Enter your roll number"<<endl;
-cin>>s1.roll;
-cout<<"Enter your branch"<<endl;
-cin>>s1.branch;
+  student s1;
+  cout<<"Enter your name"<<endl;
+  cin>>s1.name;
+  cout<<"Enter your roll number"<<endl;
+  cin>>s1.roll;
+  cout<<"Enter your branch"<<endl;
+  cin>>s1.branch;
 
-ofstream outfile;
-outfile.open("student.txt");
-outfile<<s1.name<<endl;
-outfile<<s1.roll;
-outfile<<s1.branch;
+  ofstream outfile;
+  outfile.open("student.txt");
+  outfile<<s1.name<<endl;
+  outfile<<s1.roll;
+  outfile<<s1.branch;
 
-outfile.close();
+  outfile.close();
 
-ifstream infile;
-infile.open("student.txt");
-infile>>s1.name;
-infile>>s1.roll;
-infile>>s1.branch;
+  ifstream infile;
+  infile.open("student.txt");
+  infile>>s1.name;
+  infile>>s1.roll;
+  infile>>s1.branch;
 
-cout<<s1.name<<endl;
-cout<<s1.roll<<endl;
-cout<<s1.branch<<endl;
+  cout<<s1.name<<endl;
+  cout<<s1.roll<<endl;
+  cout<<s1.branch<<endl;
 
-infile.close();
+  infile.close();
 return 0;
 
 }
