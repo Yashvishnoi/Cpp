d9607a59146a4d048cf46deabc2a1a2e732ac927
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-22 12:15:50 +0530

    Update abdul95.cpp

--- abdul95.cpp
+++ abdul95.cpp
@@ -6,7 +6,7 @@ struct point
 };
 int main()
 {
-  struct point ar[10];
+  struct point ar[10];// by creating object of array[10] here now 10 objects are created
   ar[0].x=10;
   ar[0].y=20;
   cout<<ar[0].x<<"\t"<<ar[0].y<<endl;
