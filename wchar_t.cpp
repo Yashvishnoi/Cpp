a32c112ce4688055d2046db9be44d619bf647818
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-22 13:57:59 +0530

    Update wchar_t.cpp

--- wchar_t.cpp
+++ wchar_t.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-  char c[]={"YASH VISHNOI"};
+  char c[]={"YASH VISHNOI"};// character type array string
   cout<<c<<endl;
 
-  wchar_t wa[]={L"HEY BABA"};
+  wchar_t wa[]={L"HEY BABA"};// wide char type array string
   wcout<<wa<<endl;
 return 0;
 }
