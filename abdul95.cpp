e91893d7f4b0c68519e436ce599152a652f5dffa
Yashvishnoi <jivishnoi@gmail.com>
2020-02-22 12:12:23 +0530

    Array of structure

---
+++ abdul95.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+struct point
+{
+int x,y;
+};
+int main()
+{
+struct point ar[10];
+ar[0].x=10;
+ar[0].y=20;
+cout<<ar[0].x<<"\t"<<ar[0].y<<endl;
+return 0;
+}
