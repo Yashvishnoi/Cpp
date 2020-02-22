2e8254fce2210b307ed1edd21d8efae168ef1540
Yashvishnoi <jivishnoi@gmail.com>
2020-02-22 12:31:11 +0530

    structure to a pointer

---
+++ structpointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+struct point
+{
+int x,y;
+};
+int main()
+{
+struct point p1={10,20};
+struct point *p2=&p1;
+cout<<p2->x<<"\t"<<p2->y<<endl;
+return 0;
+}
