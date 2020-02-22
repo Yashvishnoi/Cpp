dd1d871054012c0ca6b142b3a6ddd87450206378
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-22 12:34:12 +0530

    Update structpointer.cpp

--- structpointer.cpp
+++ structpointer.cpp
@@ -7,7 +7,7 @@ struct point
 int main()
 {
   struct point p1={10,20};
-  struct point *p2=&p1;
-  cout<<p2->x<<"\t"<<p2->y<<endl;
+  struct point *p2=&p1;// p2 is pointer to structure p1
+  cout<<p2->x<<"\t"<<p2->y<<endl;//Accessing structure member using structure pointer
   return 0;
 }
