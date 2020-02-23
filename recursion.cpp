c8aca768a66909f717089531b0f9e73b65a7cdb5
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-23 11:02:57 +0530

    Update recursion.cpp

--- recursion.cpp
+++ recursion.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int fun(int x)
 {
-int f;
-if(x==1)
-return 1;
-else
-f=x*fun(x-1);
-return f;
+  int f;
+  if(x==1)
+  return 1;
+  else
+  f=x*fun(x-1);
+  return f;
 }
 int main()
 {
-int a,b,c;
-cin>>b;
-a=fun(b);
-cout<<"Factorial of number is "<<a<<endl;
+  int a,b,c;
+  cin>>b;
+  a=fun(b);
+  cout<<"Factorial of number is "<<a<<endl;
 return 0;
 }
