ddd3635cb9df14be980a10ed2d7cb55ed7eac05d
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-23 12:36:37 +0530

    Update copyConstructor.cpp

--- copyConstructor.cpp
+++ copyConstructor.cpp
@@ -2,29 +2,29 @@
 using namespace std;
 class stack
 {
-private:
-int x,y;
-public:
-stack(int x,int y)
-{
-this-> x=x;
-this-> y=y;
+  private:
+  int x,y;
+  public:
+  stack(int x,int y)
+{ 
+  this-> x=x;
+  this-> y=y;
 }
 
 stack(const stack &p1)
 {
-x=p1.x;
-y=p1.y;
+  x=p1.x;
+  y=p1.y;
 }
 
 int getx()
 {
-return x;
+  return x;
 }
 
 int gety()
 {
-return y;
+  return y;
 }
 };
 
@@ -32,9 +32,9 @@ return y;
 
 int main()
 {
-stack s1(10,20);
-stack s2=s1;
-cout<<"s1.x="<<s1.getx()<<endl<<"s1.y="<<s1.gety()<<endl;
-cout<<"s2.x="<<s2.getx()<<endl<<"s2.y="<<s2.gety()<<endl;
+  stack s1(10,20);
+  stack s2=s1;
+  cout<<"s1.x="<<s1.getx()<<endl<<"s1.y="<<s1.gety()<<endl;
+  cout<<"s2.x="<<s2.getx()<<endl<<"s2.y="<<s2.gety()<<endl;
 return 0;
 }
