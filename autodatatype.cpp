1f8adf0bd4b66b3b47c60b2cfc596c93400c63f0
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-24 22:48:03 +0530

    Update autodatatype.cpp

--- autodatatype.cpp
+++ autodatatype.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 float fun()
 {
-return  2.5f;
+  return  2.5f;
 }
 int main()
 {
-double d=12.3;
-int i=9;
-auto x=2*d+i;
-//auto x=fun();
-cout<<x<<endl;
+  double d=12.3;
+  int i=9;
+  auto x=2*d+i;
+  //auto x=fun();
+  cout<<x<<endl;
 return 0;
 }
