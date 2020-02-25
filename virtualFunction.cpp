a41a52ec122ee7b4981c160f3a26c698e6a91dab
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-26 00:39:52 +0530

    Update virtualFunction.cpp

--- virtualFunction.cpp
+++ virtualFunction.cpp
@@ -4,55 +4,55 @@ using namespace std;
 
 class number
 {
-protected:
-int val;
-public:
-void setval(int i){val=i;}
-virtual int getval()=0;// getval() is a pure virtual function
+  protected:
+  int val;
+  public:
+  void setval(int i){val=i;}
+  virtual int getval()=0;// getval() is a pure virtual function
 
 };
 
 class dectype:public number
 {
-public:
-int getval()
-{
-cout<<"dectype "<<val<<endl;
-}
+  public:
+  int getval()
+  {
+    cout<<"dectype "<<val<<endl;
+  }
 };
 
 class hextype:public number
 {
-public:
-int getval()
-{
-cout<<"hextype "<<val<<endl;
-}
+  public:
+  int getval()
+  {
+    cout<<"hextype "<<val<<endl;
+  }
 };
 
 class octtype:public number
 {
-public:
-int getval()
-{
-cout<<"Octval "<<val<<endl;
-}
+  public:
+  int getval()
+  {
+    cout<<"Octval "<<val<<endl;
+  }
 };
 
 int main()
 {
 
-dectype d;
-octtype o;
-hextype h;
-d.setval(10);// displays 10-hexdecimal
-d.getval();
+  dectype d;
+  octtype o;
+  hextype h;
+  d.setval(10);// displays 10-hexdecimal
+  d.getval();
 
-o.setval(10);//display 10 octadecimal
-o.getval();
+  o.setval(10);//display 10 octadecimal
+  o.getval();
 
-h.setval(10);// displays 10 hexadecimal
-h.getval();
+  h.setval(10);// displays 10 hexadecimal
+  h.getval();
 
 return 0;
 }
