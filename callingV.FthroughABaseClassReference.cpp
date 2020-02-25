b94d2f15410b15b1b7b3d16f89206b78b46ea54a
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-25 23:54:45 +0530

    Update callingV.FthroughABaseClassReference.cpp

--- callingV.FthroughABaseClassReference.cpp
+++ callingV.FthroughABaseClassReference.cpp
@@ -2,45 +2,45 @@
 using namespace std;
 class base
 {
-public:
-virtual void fun()
-{
-cout<<"This is base class function"<<endl;
-}
+  public:
+  virtual void fun()
+  {
+    cout<<"This is base class function"<<endl;
+  }
 };
 
 class derived:public base
 {
-public:
-void fun()
-{
-cout<<"This is derived1 class function"<<endl; 
-}
+  public:
+  void fun()
+  {
+    cout<<"This is derived1 class function"<<endl; 
+  }
 };
 
 class derived2:public base
 {
-public:
-void fun()
-{
-cout<<"This is derived2 class function"<< endl;
-}
+  public:
+  void fun()
+  {
+    cout<<"This is derived2 class function"<< endl;
+  }
 };
 //Use a base class reference parameter
 void f(base &r)
-{
-r.fun();
-}
+  {
+    r.fun();
+  }
 
 int main()
 {
-base b;// pass a base class object to f()
-derived d1;// pass a derived class object to f()
-derived2 d2;// pass a derived2 class object f()
+  base b;// pass a base class object to f()
+  derived d1;// pass a derived class object to f()
+  derived2 d2;// pass a derived2 class object f()
 
-f(b);
-f(d1);
-f(d2);
+  f(b);
+  f(d1);
+  f(d2);
 
 return 0;
 }
