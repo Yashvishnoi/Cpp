598ec37161db28c7308767039838e54c5f61f1bd
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-26 23:35:02 +0530

    Update useOf*operator.cpp

--- useOf*operator.cpp
+++ useOf*operator.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-int source,target;
-int *m;
-source=10;
-m=&source;
-target=*m;
-cout<<target<<endl;
+  int source,target;
+  int *m;
+  source=10;
+  m=&source;
+  target=*m;
+  cout<<target<<endl;
 return 0;
 }
