c2a40f989007a6810ec559b647ec8944125398ba
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-26 23:35:37 +0530

    Update multipleIndirection.cpp

--- multipleIndirection.cpp
+++ multipleIndirection.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main()
 {
-int *p,**q,x;
-x=10;
-p=&x;
-q=&p;
-cout<<"Value of x"<<x<<endl;
-cout<<"Value we get by using pointer p "<<*p<<endl;
-cout<<"Value we get by using pointer q"<<**q<<endl;
+  int *p,**q,x;
+  x=10;
+  p=&x;
+  q=&p;
+  cout<<"Value of x"<<x<<endl;
+  cout<<"Value we get by using pointer p "<<*p<<endl;
+  cout<<"Value we get by using pointer q"<<**q<<endl;
 return 0;
 }
