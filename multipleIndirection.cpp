9126efe8bc20518b5675a96908dd7479af6ce599
Yashvishnoi <jivishnoi@gmail.com>
2020-02-26 23:31:52 +0530

    use of ** pointer

---
+++ multipleIndirection.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int *p,**q,x;
+x=10;
+p=&x;
+q=&p;
+cout<<"Value of x"<<x<<endl;
+cout<<"Value we get by using pointer p "<<*p<<endl;
+cout<<"Value we get by using pointer q"<<**q<<endl;
+return 0;
+}
