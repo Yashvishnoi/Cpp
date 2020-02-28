ee90d61b8e8a1116085e42e73435ad94e51110f5
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-29 00:20:17 +0530

    Update friendFunction.cpp

--- friendFunction.cpp
+++ friendFunction.cpp
@@ -3,32 +3,34 @@ using namespace std;
 
 class base
 {
-private:
-int a;
-int b;
+ private:
+ int a;
+ int b;
 
-public:
-friend int sum (base n);
- void setValue(int i,int j);
+ public: 
+  friend int sum (base n);
+  void setValue(int i,int j);
 
 };
 // Sum is not a member function of a class
+
 int sum(base n)
 {
+
  // sum is a friend function so it can directly access a and b
-return n.a+n.b;
+
+ return n.a+n.b;
 }
 void base::setValue(int i,int j)
 {
-a=i;
-b=j;
+ a=i;
+ b=j;
 }
 int main()
 {
-base obj;
-obj.setValue(10,20);
-sum(obj);
-cout<<sum(obj)<<endl;
+ base obj;
+ obj.setValue(10,20);
+ sum(obj);
+ cout<<sum(obj)<<endl;
 return 0;
 }
-
