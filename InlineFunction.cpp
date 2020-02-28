7f2f8f7937d30a57d8db41d8c42772e5781318b1
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-02-29 01:55:13 +0530

    Update InlineFunction.cpp

--- InlineFunction.cpp
+++ InlineFunction.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 class base
 {
-private:
-int a,b;
-public:
-void init(int i,int j);
-void display();
+  private:
+  int a,b;
+  public:
+  void init(int i,int j);
+  void display();
 };
 
 void base::init(int i,int j)
 {
-a=i;
-b=j;
+  a=i;
+  b=j;
 }
 
 void base::display()
 {
-cout<<a<<" "<<b<<endl; 
+  cout<<a<<" "<<b<<endl; 
 }
 
 int main()
 {
-base x;
-x.init(10,20);
-x.display();
+  base x;
+  x.init(10,20);
+  x.display();
 return 0;
 }
