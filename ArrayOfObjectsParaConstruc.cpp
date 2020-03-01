9c643dc188404ebba7761d344bb7807fe4b3b15b
Yash vishnoi <55763060+Yashvishnoi@users.noreply.github.com>
2020-03-01 12:57:39 +0530

    Update ArrayOfObjectsParaConstruc.cpp

--- ArrayOfObjectsParaConstruc.cpp
+++ ArrayOfObjectsParaConstruc.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 class base
 {
-int x;
-public:
-base(int j)
-{
-x=j;
-}
-int getvalue()
-{
-return x;
-}
+  int x;
+  public:
+  base(int j) 
+  {
+    x=j;
+  }
+  int getvalue()  
+  {
+    return x;
+  }
 };
 
 int main()
 {
-base obj[3]={20,30,40};
-int i;
+  base obj[3]={20,30,40};
+  int i;
 
-for( i=0;i<3;i++)
-{
-cout<<obj[i].getvalue()<<endl;
-}
+  for( i=0;i<3;i++)
+  {
+    cout<<obj[i].getvalue()<<endl;  
+  }
 return 0;
 }
