ba4a374e834eed067778d675da725cbf622eb981
Yashvishnoi <jivishnoi@gmail.com>
2020-03-02 23:19:59 +0530

    Commented the program

--- ConstructorOverloaded.cpp
+++ ConstructorOverloaded.cpp
@@ -9,12 +9,12 @@ int x;
 
 public:
 
-pow()
+pow()//Non parameterized constructor
 {
 x=0;
 }
 
-pow(int i)
+pow(int i)//Parameterized constructor
 {
 x=i;
 }
@@ -31,39 +31,45 @@ return x;
 
 int main()
 {
-pow p1[]={1,12,123,1234,12345};
-pow p2[5];
+pow p1[]={1,12,123,1234,12345};// initialized 
+pow p2[5];// Non-initialized
 pow *p;
 int i;
 
+// show pow of p1
 for(i=0;i<5;i++)
 {
 cout<<"Through parameterized constructor"<<p1[i].get()<<"\n";
 }
-
+ //set pow of p2
 p2[0].set(1);
 p2[1].set(18);
 p2[2].set(19);
 p2[3].set(15);
 p2[4].set(100);
 
+// show pow of p2
 for(i=0;i<5;i++)
 {
 cout<<"Non parameterized constructor "<<p2[i].get()<<"\n";
 }
 
+//dynamically allocate an array
 p=new pow[5];
 
+// initialize dynamic array with pow of p1
 for(i=0;i<5;i++)
 {
 p[i].set(p1[i].get());
 }
 
+// show powers of p1
+
 for(i=0;i<5;i++)
 {
 cout<<"When memory is dynamically allocated "<<p[i].get()<<"\n";
 }
-
+// Deallocating memory
 delete [] p;
 
 return 0;
