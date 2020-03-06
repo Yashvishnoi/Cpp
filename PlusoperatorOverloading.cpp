65d65a81bf04df83ce50660c9ff2dc0615b86e04
Yashvishnoi <jivishnoi@gmail.com>
2020-03-06 23:25:37 +0530

    + operator overloading for langitude and longitude

---
+++ PlusoperatorOverloading.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+ 
+class loc
+{
+
+private:
+
+int longitude;
+int latitude;
+
+public:
+
+loc() {}
+
+loc(int lo,int la)
+{
+longitude=lo;
+latitude=la;
+}
+
+void display()
+{
+cout<<longitude <<" longitude " <<latitude<<" latitude "<<endl;
+} 
+
+loc operator+(loc obj2);
+
+};
+
+
+loc loc::operator+(loc obj2)
+{
+loc temp;
+temp.longitude=this->longitude+obj2.longitude;
+temp.latitude=this->latitude+obj2.latitude;
+return temp;
+}
+
+int main()
+{
+loc obj1(10,20);
+loc obj2(10,20);
+loc obj3;
+obj3=obj1+obj2;
+obj3.display();
+return 0;
+}
