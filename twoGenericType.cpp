f973936a3215b2c5936b1157f5c044064d789a1c
Yashvishnoi <jivishnoi@gmail.com>
2020-03-15 16:42:00 +0530

    function with two generic type

--- twoGenericType.cpp
+++ twoGenericType.cpp
@@ -3,11 +3,14 @@ using namespace std;
 template<class X, class Y>
 void fun(X x, Y y)
 {
-cout<<x<<"\n"<<y;
+	cout<<"\n"<<x<<"\n"<<y;
 } 
 
 int main()
 {
-fun(10,"I Love you");
+
+	fun(10,"I Love you");
+	fun(10.014,198L);
+
 return 0;
 }
