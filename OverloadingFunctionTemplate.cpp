cd73f5228ceaf3b923f5708fe147628cd285aa4b
Yashvishnoi <jivishnoi@gmail.com>
2020-03-15 17:13:37 +0530

    Overloading funtion template

--- OverloadingFunctionTemplate.cpp
+++ OverloadingFunctionTemplate.cpp
@@ -4,16 +4,19 @@ using namespace std;
 template<class T> 
 void fun(T a)
 {
-cout<<"When function fun(&a) is called  "<<a;
+	cout<<"When function fun(&a) is called  "<<a;
 }
+
 template<class X, class Y> 
 void fun(X x, Y y)
 {
-cout<<"\n"<<"When function fun(X &x, Y &y) is called "<<x<<" and "<<y<<"\n";
+	cout<<"\n"<<"When function fun(X &x, Y &y) is called "<<x<<" and "<<y<<"\n";
 }
 int main()
 {
-fun(10);
-fun(10,20.5f);
+
+	fun(10);
+	fun(10,20.5f);
+
 return 0;
 }
