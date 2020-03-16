c917ce573f136347504d05bc115618753a19d1cd
Yashvishnoi <jivishnoi@gmail.com>
2020-03-16 23:59:06 +0530

    Generic classes

--- GenericClass.cpp
+++ GenericClass.cpp
@@ -4,28 +4,33 @@ using namespace std;
 template<class T , class U>
 class myclass
 {
-private:
-T X;
-U Y;
-public:
-myclass(T x1, U x2)
-{
-X=x1;
-Y=x2;
-}
-void show()
-{
-cout<<" "<<X<<"\n "<<Y<<"\n";
-}
+	private:
+	T X;
+	U Y;
+
+	public:
+	myclass(T x1, U x2)
+	{
+		X=x1;
+		Y=x2;
+	}
+
+	void show()
+	{
+		cout<<" "<<X<<"\n "<<Y<<"\n";
+
+	}
 };
 
 
 
 int main()
 {
-myclass<int,double> obj1(10,10.2);
-myclass<char,char*> obj2('Y',"I Love Budwisier");
-obj1.show();
-obj2.show();
+
+	myclass<int,double> obj1(10,10.2);
+	myclass<char,char*> obj2('Y',"I Love Budwisier");
+	obj1.show();
+	obj2.show();
+
 return 0; 
 }
