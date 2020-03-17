02858eb3b5e6499a8845ba16474ec07345268e06
Yashvishnoi <jivishnoi@gmail.com>
2020-03-17 20:26:31 +0530

    practice on same example on generic class

--- prctice.cpp
+++ prctice.cpp
@@ -6,27 +6,27 @@ using namespace std;
 template<class t1, class t2>
 class myclass
 {
-private:
-t1 a;
-t2 b;
-public:
-myclass(t1 x , t2 y)
-{
-a=x;
-b=y;
-}
+	private:
+	t1 a;
+	t2 b;
+	public:
+	myclass(t1 x , t2 y)
+	{
+		a=x;
+		b=y;
+	}
 
-void show()
-{
-cout<<a<<b;
-}
+	void show()
+	{
+		cout<<"\n" <<a<<"\n"<<b;
+	}
 };
 
 int main()
 {
-myclass<int , float> obj1(10,20.3);
-myclass<char , string>obj2('A',"I am yash baba");
-obj1.show();
-obj2.show();
+	myclass<int , float> obj1(10,20.3);
+	myclass<char , string>obj2('A',"I am yash baba");
+	obj1.show();
+	obj2.show();
 return 0;
 }
